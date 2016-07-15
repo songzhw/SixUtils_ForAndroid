ae5b47300d262d06f19323c0f238e133b56a7521
songzhw <songzhw2012@gmail.com>
2016-07-15 15:05:04 -0400

    update the library

--- Permission6/app/src/main/java/ca/six/util/demo/MainActivity.java
+++ Permission6/app/src/main/java/ca/six/util/demo/MainActivity.java
@@ -1,15 +1,43 @@
 package ca.six.util.demo;
 
+import android.Manifest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import ca.six.util.IAfterDo;
+import ca.six.util.Permission6;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, IAfterDo {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        findViewById(R.id.btn_main).setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View view) {
+        Permission6.executeWithPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE, this);
+    }
+
+    @Override
+    public void doAfterPermission() {
+        Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void userDenyPermission() {
+        Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Permission6.destory();
     }
 }
--- Permission6/library/src/main/java/ca/six/util/Permission6.java
+++ Permission6/library/src/main/java/ca/six/util/Permission6.java
@@ -7,19 +7,20 @@ import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 
-import java.lang.annotation.Target;
-
 /**
  * Created by songzhw on 2016-07-15
  */
 public class Permission6 extends Activity{
     private static IAfterDo doable;
+    public static final int REQUEST_CODE = 3;
 
     public static void executeWithPermission(Activity from, String permission, IAfterDo listener) {
         doable = listener;
-        if (ContextCompat.checkSelfPermission(from, permission) != PackageManager.PERMISSION_GRANTED) {
+        if (ContextCompat.checkSelfPermission(from, permission) == PackageManager.PERMISSION_GRANTED) {
+            System.out.println("szw 11");
             doable.doAfterPermission();
         } else {
+            System.out.println("szw 22");
             Intent it = new Intent(from, Permission6.class);
             it.putExtra("goal_permission", permission);
             from.startActivity(it);
@@ -27,7 +28,7 @@ public class Permission6 extends Activity{
     }
 
     // =================================================================
-    public void destory(){
+    public static void destory(){
         Permission6.doable = null;
     }
 
@@ -35,17 +36,21 @@ public class Permission6 extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         String permission = getIntent().getStringExtra("goal_permission");
-        ActivityCompat.requestPermissions(this, new String[]{permission}, 13);
+        ActivityCompat.requestPermissions(this, new String[]{permission}, REQUEST_CODE);
+        System.out.println("szw || 333");
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if(requestCode == 13 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+        if(requestCode == REQUEST_CODE && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            System.out.println("szw || 444");
             doable.doAfterPermission();
         } else {
+            System.out.println("szw || 555");
             doable.userDenyPermission();
         }
+        this.finish();
     }
 
 

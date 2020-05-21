4b13ea58a179edbffe60f2b0490dddf6d15059e5
songzhw <songzhw2012@gmail.com>
2020-05-21 17:02:24 -0400

    timeout function

--- SixTimeout/app/src/main/java/ca/six/timeout/demo/MainActivity.kt
+++ SixTimeout/app/src/main/java/ca/six/timeout/demo/MainActivity.kt
@@ -1,12 +1,37 @@
 package ca.six.timeout.demo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        println("szw start : ${System.currentTimeMillis()}")
+        timeout(6000) {
+            println("szw 6 second timeout: ${System.currentTimeMillis()}")
+        }
+
+        timeout(4000) {
+            println("szw 4 second timeout: ${System.currentTimeMillis()}")
+        }
     }
+}
+
+fun timeout(delayInMillSec: Long, job: () -> Unit) {
+    val timeoutId = 12345
+    val handler = object : Handler(Looper.getMainLooper()) {
+        override fun handleMessage(msg: Message) {
+            if (msg.what == timeoutId) {
+                job()
+            }
+        }
+    }
+
+    handler.sendEmptyMessageDelayed(timeoutId, delayInMillSec)
 }
